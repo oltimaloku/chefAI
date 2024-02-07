5296b656c06ec9ab2e411b33895df5ea091cfd05
oltimaloku <oltimaloku555@gmail.com>
2024-02-07 13:45:53 -0800

    Initial Commit

---
+++ ChefAI/ChefAIApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChefAIApp.swift
+//  ChefAI
+//
+//  Created by Olti Maloku on 2024-02-07.
+//
+
+import SwiftUI
+
+@main
+struct ChefAIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
