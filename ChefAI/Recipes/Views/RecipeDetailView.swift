44d5898aec9d0d0838af3cfdc9d9c2bd5c723448
oltimaloku <oltimaloku555@gmail.com>
2024-03-01 15:55:45 -0800

    Recipes page

--- ChefAI/Recipes/Views/RecipeDetailView.swift
+++ ChefAI/Recipes/Views/RecipeDetailView.swift
@@ -10,7 +10,16 @@ import SwiftUI
 struct RecipeDetailView: View {
     var recipe: Recipe
     var body: some View {
-        Text(recipe.name)
+        VStack {
+            Text(recipe.name).fontWeight(/*@START_MENU_TOKEN@*/.bold/*@END_MENU_TOKEN@*/).font(/*@START_MENU_TOKEN@*/.title/*@END_MENU_TOKEN@*/)
+            VStack {
+                Text("Instrutions")
+                Text(recipe.instructions)
+            }
+            
+        }
+        
+        
     }
 }
 
