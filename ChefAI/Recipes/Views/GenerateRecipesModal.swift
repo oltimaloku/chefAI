9cca7e9aaa4103c6d5a60155466dc273ad4dccb1
oltimaloku <oltimaloku555@gmail.com>
2024-03-01 16:14:05 -0800

    added generate recipes button to recipes page

---
+++ ChefAI/Recipes/Views/GenerateRecipesModal.swift
@@ -0,0 +1,18 @@
+//
+//  GenerateRecipesModal.swift
+//  ChefAI
+//
+//  Created by Olti Maloku on 2024-03-01.
+//
+
+import SwiftUI
+
+struct GenerateRecipesModal: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    GenerateRecipesModal()
+}
--- ChefAI/Recipes/Views/RecipesView.swift
+++ ChefAI/Recipes/Views/RecipesView.swift
@@ -6,33 +6,56 @@
 //
 
 import SwiftUI
-// TODO: Consider adding this to inventory page with a top level toggle/switcher that allows you to switch between food itesm and recipes 
+// TODO: Consider adding this to inventory page with a top level toggle/switcher that allows you to switch between food items and recipes
+
 struct RecipesView: View {
     @EnvironmentObject var recipesViewModel: RecipesViewModel
     @State private var showingModal = false
+    @State private var showingGenerateRecipesModal = false
+
     var body: some View {
         NavigationView {
-            ScrollView {
-                VStack {
-                    ForEach(recipesViewModel.getRecipes) {
-                        recipe in RecipeComponent(recipe: recipe)
-                    }
-                    .navigationBarTitle("Food Items", displayMode: .large)
-                    .navigationBarItems(trailing: Button(action: {
-                        self.showingModal = true
-                    }) {
-                        Image(systemName: "plus")
-                    })
-                    .sheet(isPresented: $showingModal) {
-                        AddFoodItemModal()
-                        
+            VStack {
+                ScrollView {
+                    VStack {
+                        ForEach(recipesViewModel.getRecipes) { recipe in
+                            RecipeComponent(recipe: recipe)
+                        }
                     }
                 }
+                .navigationBarTitle("Food Items", displayMode: .large)
+                .navigationBarItems(trailing: Button(action: {
+                    self.showingModal = true
+                }) {
+                    Image(systemName: "plus")
+                })
+                .sheet(isPresented: $showingModal) {
+                    AddFoodItemModal()
                 }
+                .sheet(isPresented: $showingGenerateRecipesModal) {
+                    GenerateRecipesModal()
+                }
+
+                Spacer() // This will push the button to the bottom
+                
+                Button(action: {
+                    self.showingGenerateRecipesModal = true
+                }) {
+                    Text("Generate Recipes")
+                        .frame(maxWidth: .infinity)
+                        .padding()
+                        .background(Color.blue)
+                        .foregroundColor(Color.white)
+                        .cornerRadius(10)
+                }
+                .padding(.horizontal)
+                .padding(.bottom, 8) // Adjust padding as needed
             }
         }
+    }
 }
 
 #Preview {
     RecipesView()
 }
+
